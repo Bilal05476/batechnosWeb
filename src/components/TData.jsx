33f87c79a0a8ee0e3531b43471c4278785fe117c
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-11-03 14:56:28 +0500

    updates

--- src/components/TData.jsx
+++ src/components/TData.jsx
@@ -23,18 +23,6 @@ const TData = [
     feedBack:
       "They are so professional provide the best services to me every time, I was looking for graphic designing related work and they fulfill my need as I expected. Thank you so many buddies Keep that up.. 🚀",
   },
-  {
-    name: "Samm Dev",
-    service: "Social Media Marketing",
-    feedBack:
-      "They are so professional provide the best services to me every time, I was looking for social media-related work and they fulfill my need as I expected. Thank you so many buddies Keep that up.. 🚀",
-  },
-  {
-    name: "Abdullah Majeed",
-    service: "Graphic Designing",
-    feedBack:
-      "They are so professional provide the best services to me every time, I was looking for graphic designing related work and they fulfill my need as I expected. Thank you so many buddies Keep that up.. 🚀",
-  },
 ];
 
 export default TData;
